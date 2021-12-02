21f983cb2a422511daf61d3fa72641230ad6664c
nyannnnn <90939473+nyannnnn@users.noreply.github.com>
2021-12-02 10:42:00 -0500

    added solutions too binary search, first bad version, and search insert position

---
+++ src/First_Bad_Version.java
@@ -0,0 +1,18 @@
+class First_Bad_Version {
+    public int firstBadVersion(int n) {
+        int left = 0;
+		int right = n;
+		
+		while(left < right) {
+			int mid = left + (right - left)/2;
+			if(!(isBadVersion(mid))) {
+				left = mid + 1;
+			}
+			else {
+				right = mid;
+			}
+		}
+		
+		return left;
+    }
+}
